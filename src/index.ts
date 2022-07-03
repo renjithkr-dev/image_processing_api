ca9b610281706f4656fb120cf2bac59e35bf124a
Renjith Raveendran <>
2022-07-03 06:34:05 +0000

    Setup  morgan with winston

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello");
+import  express from "express";
+import { logger, enableDebug } from "./utilities/logger";
+import morgan from "./utilities/morganMiddleware"
+
+const app = express();
+
+const PORT: number = (process.env.NODE_PORT as unknown) as number || 3000;
+const DEBUG = process.env.NODE_ENV === "development";
+if (DEBUG) enableDebug(logger);
+
+app.use(morgan);
+
+app.get("/",(req:express.Request,res:express.Response) => {
+    res.status(200).send("Hello");
+})
+app.listen(PORT, () => {
+  logger.info("test info");
+  logger.debug("test debug");
+  logger.info("test info");
+  logger.debug("test debug1");
+});
